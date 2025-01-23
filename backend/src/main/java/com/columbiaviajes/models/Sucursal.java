7c8318a93adf8d1e9f290c90f6b35dc8e5044d3f
D-Malex <3matias.sm@gmail.com>
2025-01-23 16:16:31 -0300

    fix(API): el endpoint de sucursales no funcionaba

--- backend/src/main/java/com/columbiaviajes/models/Sucursal.java
+++ backend/src/main/java/com/columbiaviajes/models/Sucursal.java
@@ -1,14 +1,24 @@
 package com.columbiaviajes.models;
 
 import java.util.List;
-import jakarta.persistence.*;
-import lombok.experimental.FieldDefaults;
-import lombok.AllArgsConstructor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
 @Table(name="Sucursal")
