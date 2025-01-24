2247eaad9a1741d2317e5ca1962fc81a46c5905c
D-Malex <3matias.sm@gmail.com>
2025-01-23 21:56:35 -0300

    feat(Seller): Agrego el footer

--- frontend/src/pages/seller/Home.jsx
+++ frontend/src/pages/seller/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Footer from "../Footer";
 
 function Home() {
   return (
     <div>
       <NavBar />
       <h1>Welcome SELLER</h1>
+      <Footer />
     </div>
   );
 }
