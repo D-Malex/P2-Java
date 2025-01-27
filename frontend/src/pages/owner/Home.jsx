77db65f9343023ead7b45f6659b52742fe44d749
D-Malex <3matias.sm@gmail.com>
2025-01-27 17:08:14 -0300

    feat(owner): Estructura inicial

--- frontend/src/pages/owner/Home.jsx
+++ frontend/src/pages/owner/Home.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Footer from "../Footer";
+import "./styles/Home.css";
+
 function Home() {
-  return <h1>Welcome OWNER</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1>Welcome OWNER</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
---
+++ frontend/src/pages/owner/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaSignOutAlt } from "react-icons/fa"; 
+import "./styles/NavBar.css";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("email");
+    navigate("/login");
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/home"> <img src="../../../public/columbia-viajesv2.png" alt="Logo"/> </a>
+      </div>
+
+      <div className="navbar-links">
+        <Link to="/home"><b>Inicio</b></Link>
+        <Link to="/Sucursales"><b>Sucursales</b></Link>
+        <Link to="/flights"><b>Vuelos</b></Link>
+        <Link to="/hotels"><b>Hoteles</b></Link>
+        <Link to="/registry/sales"><b>Registro de ventas</b></Link>
+        <Link to="/personel"><b>Personal</b></Link>
+        <Link to="/profile"><b>Mi perfil</b></Link>
+      </div>
+      <div id="navbar-logout">
+        <button id="logout-button" onClick={handleLogout}> 
+          <FaSignOutAlt size={20} />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
