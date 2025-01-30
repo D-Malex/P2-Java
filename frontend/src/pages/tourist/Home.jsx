c501574a76d4c4118da95603e0513f28de48df04
D-Malex <3matias.sm@gmail.com>
2025-01-30 01:31:45 -0300

    style(tourist): Mejorando la visual del home

--- frontend/src/pages/tourist/Home.jsx
+++ frontend/src/pages/tourist/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import api from "../../utils/api";
 import "./styles/Home.css";
 
@@ -10,6 +11,7 @@ function Home() {
     const fetchBranches = async () => {
       try {
         const response = await api.get("/sucursales");
+        console.log("Se trajeron exitosamente las sucursales.");
         setBranches(response.data.slice(0, 3));
       } catch (error) {
         console.error("Error al cargar las sucursales:", error);
@@ -22,7 +24,7 @@ function Home() {
   return (
     <>
       <div className="hero-section">
-        <h1 className="hero-title">Hola, {usuario.nombre}</h1>
+        <h1>Hola, {usuario.nombre}</h1>
       </div>
 
       <section className="about-us">
@@ -32,27 +34,29 @@ function Home() {
           través de viajes personalizados. Queremos conectar personas con los
           lugares más hermosos del mundo.
         </p>
+        <p>
+          Desde 1964 cumpliendo el sueño de muchos viajeros! 
+          Asesoramiento personalizado por profesionales expertos en viajes.
+        </p>
       </section>
 
       <section className="branches">
-        <h2>Sucursales</h2>
+        <h2>¿Donde podes encontrarnos?</h2>
+
         <div className="cards-container">
           {branches.map((branch) => (
-            <div className="card" key={branch.id_Sucursal}>
-              <img
-                src={`sucursal${branch.id_sucursal}.jpg`} // Suposición de imágenes nombradas por ID
-                alt={`Sucursal ${branch.id_sucursal}`}
-              />
+            <div className="card" key={branch.id_sucursal}>
+              <div className="map-icon-container">
+              <FaMapMarkerAlt className="map-icon" />
+            </div>
               <h3>{`Sucursal ${branch.id_sucursal}`}</h3>
-              <p>Dirección: {branch.direccion}</p>
-              <p>Teléfono: {branch.telefono}</p>
+              <p><b>Dirección:</b> {branch.direccion}</p>
+              <p><b>Teléfono:</b> {branch.telefono}</p>
             </div>
           ))}
         </div>
-        <button
-          className="view-more"
-          onClick={() => (window.location.href = "/sucursales")}
-        >
+
+        <button className="view-more" onClick={() => (window.location.href = "/sucursales")}>
           Ver más sucursales
         </button>
       </section>
