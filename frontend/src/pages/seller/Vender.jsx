457440ed18ba06ee2ea531ae4cdf604e12e75113
D-Malex <3matias.sm@gmail.com>
2025-01-30 16:57:41 -0300

    style(seller): Vender

--- frontend/src/pages/seller/Vender.jsx
+++ frontend/src/pages/seller/Vender.jsx
@@ -10,8 +10,8 @@ const Vender = () => {
   const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
   const [vueloSeleccionado, setVueloSeleccionado] = useState(null);
   const [hotelSeleccionado, setHotelSeleccionado] = useState(null);
-  const [pension, setPension] = useState("SIMPLE");
-  const [claseVuelo, setClaseVuelo] = useState("ECONOMICA");
+  const [pension, setPension] = useState("MEDIA");
+  const [claseVuelo, setClaseVuelo] = useState("TURISTA");
   const [fechaRetorno, setFechaRetorno] = useState("");
   const [precio, setPrecio] = useState(0);
 
@@ -19,7 +19,6 @@ const Vender = () => {
   
   useEffect(() => {
     const fetchSucursal = async () => {
-        const vendedor = JSON.parse(localStorage.getItem("usuario"));
         const response = await api.get(`/sucursales/${vendedor.id_sucursal}`);
         setSucursal(response.data);
     };
@@ -63,7 +62,7 @@ const Vender = () => {
     let basePrecio = vueloSeleccionado.plazasTurista * 10; // Precio base por vuelo
 
     if (claseVuelo === "PRIMERA") basePrecio *= 2;
-    if (pension === "MEJOR") basePrecio += 200;
+    if (pension === "COMPLETA") basePrecio += 200;
 
     // Calcula días extra
     const diasExtra = calcularDiasExtra(vueloSeleccionado.fecha, fechaRetorno);
@@ -79,7 +78,7 @@ const Vender = () => {
     if (!usuarioSeleccionado?.id_usuario || !vueloSeleccionado?.id_vuelo || !hotelSeleccionado?.id_hotel || !sucursal?.id_sucursal) {
       alert("Algunos datos esenciales no están seleccionados. Verifica la información ingresada.");
       return;
-  }
+    }
 
     const fechaActual = new Date();
     const fechaInicio = new Date(vueloSeleccionado.fecha);
@@ -94,33 +93,28 @@ const Vender = () => {
       return;
     }
 
-
-    // Crear el viaje
-    const viaje = {
-      sucursal: sucursal, // Solo ID
-      usuario: usuarioSeleccionado, // Solo ID
-      hotel: hotelSeleccionado, // Solo ID
-      pensionHotel: pension,
-      vuelo: vueloSeleccionado, // Solo ID
-      claseVuelo,
-      fechaLlegada: vueloSeleccionado.fecha,
-      fechaRetorno,
-      precio,
-  };
-
-    api.post("/viajes/new", viaje).then((viajeResponse) => {
-      // Crear la venta asociada al viaje
-      const venta = {
-        vendedor,
-        viaje: viajeResponse.data,
-        fechaVenta: new Date().toISOString(),
+    if(confirm("¿Seguro que quiere vender este viaje? Verifique los datos.")) {
+      // Crear el viaje
+      const viaje = {
+        sucursal: sucursal,
+        usuario: usuarioSeleccionado,
+        hotel: hotelSeleccionado,
+        pensionHotel: pension,
+        vuelo: vueloSeleccionado,
+        claseVuelo,
+        fechaLlegada: vueloSeleccionado.fecha,
+        fechaRetorno,
+        precio,
       };
-      console.log("Viaje creado" + viaje);
 
-      api.post("/ventas/new", venta).then(() => {
-        alert("Venta realizada exitosamente");
+      api.post("/viajes/new", viaje).then((viajeResponse) => {
+        // Crear la venta asociada al viaje
+        const venta = { vendedor, viaje: viajeResponse.data, fechaVenta: new Date().toISOString(),};
+        console.log("Viaje creado" + viaje);
+
+        api.post("/ventas/new", venta).then(() => { alert("Venta realizada exitosamente"); });
       });
-    });
+    }
   };
 
 
@@ -128,95 +122,80 @@ const Vender = () => {
   return (
     <>
       <div className="vender-container">
-    <h1>Vender Viaje</h1>
-    <div className="form-group">
-  <label>Seleccione un usuario:</label>
-  <input className="input_especial"
-    list="usuarios"
-    onChange={(e) => {
-      const selectedUser = usuarios.find((u) => u.email === e.target.value);
-      setUsuarioSeleccionado(selectedUser);
-    }}
-  />
-  <datalist id="usuarios">
-    {usuarios.map((usuario) => (
-      <option key={usuario.email} value={usuario.email}>
-        {usuario.nombre} {usuario.apellido} ({usuario.email})
-      </option>
-    ))}
-  </datalist>
-</div>
-    <div className="form-group">
-      <label>Seleccione un vuelo:</label>
-      <select onChange={(e) => handleVueloChange(e.target.value)}>
-        <option value="">-- Seleccione un vuelo --</option>
-        {vuelos.map((vuelo) => (
-          <option key={vuelo.id_vuelo} value={vuelo.id_vuelo}>
-            {vuelo.origen} ➡️ {vuelo.destino} ({vuelo.fecha})
-          </option>
-        ))}
-      </select>
-    </div>
+        <h1>Vender Viaje</h1>
 
-    {vueloSeleccionado && (
-      <>
-        <div className="form-group">
-          <label>Seleccione un hotel:</label>
-          <select
-            onChange={(e) =>
-              setHotelSeleccionado(hoteles.find((h) => h.id_hotel === parseInt(e.target.value)))
-            }
-          >
-            <option value="">-- Seleccione un hotel --</option>
-            {hoteles.map((hotel) => (
-              <option key={hotel.id_hotel} value={hotel.id_hotel}>
-                {hotel.nombre} ({hotel.direccion})
+        <div>
+          <label>Seleccione un usuario:</label>
+          <input list="usuarios" onChange={(e) => {
+              const selectedUser = usuarios.find((u) => u.email === e.target.value);
+              setUsuarioSeleccionado(selectedUser);
+            }}
+          />
+          <datalist id="usuarios">
+            {usuarios.map((usuario) => (
+              <option key={usuario.email} value={usuario.email}>
+                {usuario.nombre} {usuario.apellido} ({usuario.email})
+              </option>
+            ))}
+          </datalist>
+        </div>
+        <div>
+          <label>Seleccione un vuelo:</label>
+          <select onChange={(e) => handleVueloChange(e.target.value)}>
+            <option value="">-- Seleccione un vuelo --</option>
+            {vuelos.map((vuelo) => (
+              <option key={vuelo.id_vuelo} value={vuelo.id_vuelo}>
+                {vuelo.origen} ➡️ {vuelo.destino} ({vuelo.fecha})
               </option>
             ))}
           </select>
         </div>
 
-        <div className="form-group">
-          <label>Clase de vuelo:</label>
-          <select onChange={(e) => setClaseVuelo(e.target.value)}>
-            <option value="ECONOMICA">Económica</option>
-            <option value="PRIMERA">Primera</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label>Pensión del hotel:</label>
-          <select onChange={(e) => setPension(e.target.value)}>
-            <option value="SIMPLE">Simple</option>
-            <option value="MEJOR">Mejor</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label>Fecha de retorno:</label>
-          <input class="input_especial"
-            type="date"
-            onChange={(e) => setFechaRetorno(e.target.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Días extra:</label>
-          <p>{calcularDiasExtra(vueloSeleccionado.fecha, fechaRetorno)} días</p>
-        </div>
-
-
-        <div className="form-group">
-          <button onClick={calcularPrecio}>Calcular Precio</button>
-          <p>Precio: ${precio}</p>
-        </div>
-
-        <div className="form-group">
-          <button onClick={handleVender}>Vender</button>
-        </div>
-      </>
-    )}
-  </div>
+        {vueloSeleccionado && (
+          <>
+            <div>
+              <label>Seleccione un hotel:</label>
+              <select onChange={(e) => setHotelSeleccionado(hoteles.find((h) => h.id_hotel === parseInt(e.target.value)))}>
+                <option value="">-- Seleccione un hotel --</option>
+                  {hoteles.map((hotel) => (
+                  <option key={hotel.id_hotel} value={hotel.id_hotel}>
+                    {hotel.nombre} ({hotel.direccion})
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Clase de vuelo:</label>
+              <select onChange={(e) => setClaseVuelo(e.target.value)}>
+                <option value="TURISTA">Turista</option>
+                <option value="PRIMERA">Primera</option>
+              </select>
+            </div>
+            <div>
+              <label>Pensión del hotel:</label>
+              <select onChange={(e) => setPension(e.target.value)}>
+                <option value="MEDIA">Media</option>
+                <option value="COMPLETA">Completa</option>
+              </select>
+            </div>
+            <div>
+              <label>Fecha de retorno:</label>
+              <input type="date" onChange={(e) => setFechaRetorno(e.target.value)} />
+            </div>
+            <div>
+              <label>Días extra:</label>
+              <p id="diasExtra">{calcularDiasExtra(vueloSeleccionado.fecha, fechaRetorno)} días</p>
+            </div>
+            <div>
+              <button onClick={calcularPrecio}>Calcular Precio</button>
+              <p>Precio: ${precio}</p>
+            </div>
+            <div>
+              <button onClick={handleVender}>Vender</button>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 };
