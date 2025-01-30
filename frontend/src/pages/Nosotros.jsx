4b9ca6d916ff399d5d85a705684579d697980c12
D-Malex <3matias.sm@gmail.com>
2025-01-30 02:44:26 -0300

    style(nosotros): Cambio estilos y texto del nosotros

--- frontend/src/pages/Nosotros.jsx
+++ frontend/src/pages/Nosotros.jsx
@@ -3,22 +3,33 @@ import "./styles/Nosotros.css";
 
 const Nosotros   = () => {
   return (
-    <div className="about-us-container">
+    <>
+      <div className="about-us-container">
       <h1>Acerca de Nosotros</h1>
       <p>
-        Nuestra misión es ofrecer experiencias únicas de viaje, brindando un
-        servicio de calidad y asegurando la satisfacción de nuestros clientes.
+        Desde nuestros comienzos en 1964, en Columbia Viajes hemos logrado conformar 
+        un sólido equipo de profesionales altamente capacitados, que hoy, con el respaldo 
+        de herramientas tecnológicas y procesos certificados, son los responsables de nuestro  
+        crecimiento. Confianza es nuestra mayor virtud.
       </p>
       <p>
-        Nuestro objetivo es ser la agencia líder en turismo, promoviendo destinos
-        maravillosos y un servicio excepcional. Nos dedicamos a garantizar que
-        cada viaje sea memorable, lleno de aventuras y descubrimientos.
+        Contamos con sucursales físicas en pleno centro de la ciudad de Buenos Aires, para que 
+        nos puedan conocer personalmente si así lo desean. Nuestra solvencia adquirida en estos años, 
+        forma parte de los requerimientos más exigentes para el turismo nacional e Internacional gestionando 
+        viajes corporativos, diseño de viajes a medida para pasajeros individuales y grupos a todo 
+        el mundo, así como en turismo receptivo de Argentina, donde un departamento especializado 
+        ofrece un excelente servicio con guías bilingües y prestaciones de experiencias únicas.
       </p>
       <p>
-        Te invitamos a conocer el mundo con nosotros, explorando nuevos
-        horizontes y viviendo momentos que nunca olvidarás. ¡Viajar es vivir!
+        Nuestros servicios de alta calidad y la vocación de servicio de nuestros profesionales han 
+        hecho que miles de pasajeros nos recomienden y vuelvan a elegir nuestros servicios a lo largo 
+        de estos años…
+      </p>
+      <p>
+      Sorpréndase con una experiencia de viaje diferente!!!
       </p>
     </div>
+    </>
   );
 };
 
