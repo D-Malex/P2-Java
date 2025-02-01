9379d006f7a456f3783a79bc8dc9e606b08c904f
D-Malex <3matias.sm@gmail.com>
2025-02-01 17:41:20 -0300

    feat(sucursales): ABM funcional

--- backend/middlewares/globalMiddlewares.js
+++ backend/middlewares/globalMiddlewares.js
@@ -2,9 +2,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const globalMiddlewares = (app) => {
+  // Configurar CORS con opciones específicas
+  const corsOptions = {
+    origin: ["http://localhost:5173"], 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    credentials: true,
+  };
+  
   // Middleware para habilitar CORS
-  app.use(cors());
-
+  app.use(cors(corsOptions));
+  
   // Middleware para parsear el cuerpo de las solicitudes a JSON
   app.use(bodyParser.json());
 };
